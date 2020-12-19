eed4795df0b421d254f7a66ba1df55749413e8cd
Borna Arabkhedri <borna.khedri@gmail.com>
2020-12-18 17:05:05 -0800

    new app.ui designed using Qt designer. appui.py developed with functionality of menu buttons: connect, disconnect, and about the app

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from qtTest import *
+from appui import *
+
+# def main():
+#     app = QApplication(sys.argv)
+#     win = MyWindow()
+#     win.show()
+#     sys.exit(app.exec_())
+
 
 def main():
-    app = QApplication(sys.argv)
-    win = MyWindow()
-    win.show()
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
     main()
